fc1a2cf3c539a25ac1a70b0257a15494788107c6
kececihasan <ghasankececi@gmail.com>
2025-03-04 11:17:30 +0300

    a

--- functions/urunler/searchlive.php
+++ functions/urunler/searchlive.php
@@ -13,22 +13,20 @@ if (isset($_POST['searchQuery'])) {
                             WHERE (n.UrunAdiTR LIKE '%$search%' OR n.UrunKodu LIKE '%$search%' OR m.title LIKE '%$search%') 
                             AND n.web_comtr = '1' 
                             ORDER BY n.UrunAdiTR ASC 
-                            LIMIT 10");
+                            LIMIT 10", ['search' => '%' . $search . '%']);
 
     $response = [];
     if (!empty($result)) {
         foreach ($result as $row) {
             // Get product image
-            $urunResim = $db->fetch("SELECT KResim FROM nokta_urunler_resimler WHERE UrunID = :urun_id LIMIT 1", ['UrunID' => $row['id']]);
-            $resim = !empty($urunResim['KResim']) ? $urunResim['KResim'] : 'gorsel_hazirlaniyor.jpg';
-
+          
             // Push data to the response array
             $response[] = [
                 'seo_link' => $row['seo_link'],
                 'UrunKodu' => $row['UrunKodu'],  // Now included in the SELECT query
                 'UrunAdiTR' => $row['UrunAdiTR'], // Now included in the SELECT query
                 'MarkaAdi' => $row['marka_adi'],
-                'KResim' => $resim
+                'KResim' => $row['seo_link']
             ];
         }
     }
