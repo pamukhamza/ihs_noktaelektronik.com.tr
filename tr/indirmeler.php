3ce0ea6b0c5ed229a44061cf1a3dbc7a108412cd
kececihasan <ghasankececi@gmail.com>
2025-03-04 11:28:31 +0300

    a

--- tr/indirmeler.php
+++ tr/indirmeler.php
@@ -129,9 +129,8 @@ $database = new Database();
                     if (results.length > 0) {
                         results.forEach(function(result) {
                             dropdownMenu1.append(`
-                                <a class="dropdown-item rounded-5" href="tr/urunler/${result.seo_link}">
-                                    <img src="https://noktanet.s3.eu-central-1.amazonaws.com/uploads/images/products/${result.KResim}" alt="" style="max-width: 50px; margin-right: 10px;">
-                                    <span style="font-weight:bold;">${result.UrunKodu}</span> - ${result.UrunAdiTR}
+                                <a class="dropdown-item rounded-5" href="tr/indirmeler?urun=${result.id}">
+                                    <img src="https://noktanet.s3.eu-central-1.amazonaws.com/uploads/images//${result.KResim}" alt="" style="max-width: 50px; margin-right: 10px;"> ${result.UrunAdiTR} - ${result.UrunKodu}
                                 </a>
                             `);
                         });
