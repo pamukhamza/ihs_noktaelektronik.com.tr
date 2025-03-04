93c4d8ccc97284d08498ff6d2ff4198e2bdd10ea
kececihasan <ghasankececi@gmail.com>
2025-03-04 11:08:26 +0300

    a

--- functions/urunler/searchlive.php
+++ functions/urunler/searchlive.php
@@ -2,16 +2,12 @@
 require_once '../db.php';
 $db = new Database();
 
-if (isset($_POST['searchQuery']) && isset($_POST['uye_id'])) {
+if (isset($_POST['searchQuery'])) {
     $search = $_POST['query'];
     $uye_id = $_POST['uye_id'];
 
-    // Get user price level
-    $uye = $db->fetch("SELECT fiyat FROM uyeler WHERE id = :uye_id", ['uye_id' => $uye_id]);
-    $uyeFiyat = $uye['fiyat'];
-
     // Search products
-    $result = $db->fetchAll("SELECT DISTINCT n.DSF4, n.DSF3, n.DSF2, n.DSF1, n.KSF4, n.KSF3, n.KSF2, n.KSF1, n.DOVIZ_BIRIMI, n.seo_link, n.id, m.title as marka_adi 
+    $result = $db->fetchAll("SELECT DISTINCT n.seo_link, n.id, m.title as marka_adi 
                             FROM nokta_urunler n 
                             LEFT JOIN nokta_urun_markalar m ON n.MarkaID = m.id 
                             WHERE (n.UrunAdiTR LIKE :search OR n.UrunKodu LIKE :search OR m.title LIKE :search) 
@@ -24,9 +20,6 @@ if (isset($_POST['searchQuery']) && isset($_POST['uye_id'])) {
 
     if (!empty($result)) {
         foreach ($result as $row) {
-            $fiyat = !empty($row["DSF" . $uyeFiyat]) ? $row["DSF" . $uyeFiyat] : $row["KSF" . $uyeFiyat];
-            $doviz = !empty($row["DSF" . $uyeFiyat]) ? $row["DOVIZ_BIRIMI"] : "₺";
-
             // Get product image
             $urunResim = $db->fetch("SELECT KResim FROM nokta_urunler_resimler WHERE UrunID = :urun_id LIMIT 1", ['UrunID' => $row['id']]);
             $resim = !empty($urunResim['KResim']) ? $urunResim['KResim'] : 'gorsel_hazirlaniyor.jpg';
@@ -37,8 +30,6 @@ if (isset($_POST['searchQuery']) && isset($_POST['uye_id'])) {
                 'UrunKodu' => $row['UrunKodu'],  // Add this if needed for search result
                 'UrunAdiTR' => $row['UrunAdiTR'],
                 'MarkaAdi' => $row['marka_adi'],
-                'fiyat' => number_format($fiyat, 2, ',', '.'),
-                'doviz' => $doviz,
                 'KResim' => $resim
             ];
         }
