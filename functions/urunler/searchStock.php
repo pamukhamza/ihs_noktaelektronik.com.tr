59b44e8f1c29633f1ab71665a0de3fba23d48199
kececihasan <ghasankececi@gmail.com>
2025-03-04 11:25:07 +0300

    a

--- functions/urunler/searchStock.php
+++ functions/urunler/searchStock.php
@@ -1,43 +1,36 @@
 <?php
-//searchStock.php
-// Önce veritabanı bağlantısını sağlayın
 require_once '../db.php';
-$db = new Database();
 if (isset($_POST['searchQuery'])) {
+    $db = new Database();
     $search = $_POST['searchQuery'];
-    // Search products
-    $result = $db->fetchAll(" 
-        SELECT DISTINCT n.id, n.BLKODU, n.UrunAdiTR, n.stok, 
-            (SELECT r.KResim 
-             FROM nokta_urunler_resimler r 
-             WHERE r.UrunID = n.id 
-             ORDER BY r.id ASC 
-             LIMIT 1) as KResim,
-            m.title as marka_adi 
-        FROM nokta_urunler n 
-        LEFT JOIN nokta_urun_markalar m ON n.MarkaID = m.id 
-        WHERE (n.UrunAdiTR LIKE :search OR n.BLKODU LIKE :search OR m.title LIKE :search) 
-        AND n.web_comtr = '1' 
-        ORDER BY n.UrunAdiTR ASC 
-        LIMIT 10",
-        ['search' => '%' . $search . '%']);
-    // Prepare the response
-    $response = [];
+    $result = $db->fetchAll("SELECT DISTINCT n.seo_link, n.id, n.UrunAdiTR, n.UrunKodu, m.title as marka_adi 
+                            FROM nokta_urunler n 
+                            LEFT JOIN nokta_urun_markalar m ON n.MarkaID = m.id 
+                            WHERE (n.UrunAdiTR LIKE '%$search%' OR n.UrunKodu LIKE '%$search%' OR m.title LIKE '%$search%') 
+                            AND n.web_comtr = '1' 
+                            ORDER BY n.UrunAdiTR ASC 
+                            LIMIT 10");
 
+    $response = [];
     if (!empty($result)) {
         foreach ($result as $row) {
-            // Add each result to the response array
+            $urun_id = $row['id'];
+            $urunResim = $db->fetch("SELECT KResim FROM nokta_urunler_resimler WHERE UrunID = $urun_id LIMIT 1");
+            $resim = !empty($urunResim['KResim']) ? $urunResim['KResim'] : 'gorsel_hazirlaniyor.jpg';
+
+            // Push data to the response array
             $response[] = [
-                'id' => $row['id'],
-                'UrunAdiTR' => $row['UrunAdiTR'],
-                'BLKODU' => $row['BLKODU'],
-                'stok' => $row['stok'],
-                'KResim' => $row['KResim'],
-                'marka_adi' => $row['marka_adi']
+                'seo_link' => $row['seo_link'],
+                'UrunKodu' => $row['UrunKodu'],  // Now included in the SELECT query
+                'UrunAdiTR' => $row['UrunAdiTR'], // Now included in the SELECT query
+                'MarkaAdi' => $row['marka_adi'],
+                'KResim' => $resim
             ];
         }
     }
-    // Send the response as JSON
+    header('Content-Type: application/json');
     echo json_encode($response);
+}else {
+    echo json_encode([]);
 }
 ?>
