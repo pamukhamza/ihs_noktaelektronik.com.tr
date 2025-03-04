cbe09502956feff382d4a4bc3989762b3ace2192
kececihasan <ghasankececi@gmail.com>
2025-03-04 11:14:49 +0300

    a

--- functions/urunler/searchlive.php
+++ functions/urunler/searchlive.php
@@ -1,6 +1,8 @@
 <?php
 require_once '../db.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 if (isset($_POST['searchQuery'])) {
     $db = new Database();
     $search = $_POST['searchQuery'];
