856baa747d565810aaf6366bc081e1416709717a
kececihasan <ghasankececi@gmail.com>
2025-03-05 12:18:28 +0300

    a

--- functions/get/get_favorites.php
+++ functions/get/get_favorites.php
@@ -2,14 +2,10 @@
 require_once '../db.php';
 $db = new Database();
 
-session_name("user_session");
-session_start();
 $userId = $_SESSION['id'];
 
 // Fetch favorite products
-$favoriteProducts = $db->fetchColumn("SELECT urun_id FROM nokta_uye_favoriler WHERE uye_id = :user_id", [
-    'user_id' => $userId
-]);
+$favoriteProducts = $db->fetchColumn("SELECT urun_id FROM nokta_uye_favoriler WHERE uye_id = :user_id", ['user_id' => $userId]);
 
 echo json_encode($favoriteProducts);
 ?>
