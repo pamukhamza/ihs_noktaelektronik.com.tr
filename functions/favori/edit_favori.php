157f9c9dd5e1a3c24ef8ec1dc79590cfc705289c
kececihasan <ghasankececi@gmail.com>
2025-03-05 11:57:38 +0300

    a

--- functions/favori/edit_favori.php
+++ functions/favori/edit_favori.php
@@ -2,20 +2,21 @@
 require_once '../db.php';
 $database = new Database();
 
-    $productId = $_POST['product_id'];
-    $userId = $_POST['uye_id'];
-    if(empty($userId)){
-        header("Location:../../tr/giris");
-        exit(); // Terminate script after redirect
-    }
-    else{
-        $checkStatement = $database->fetch("SELECT id FROM nokta_uye_favoriler WHERE uye_id = $userId AND urun_id = $productId");
-        if (!empty($checkStatement)) {
-            $database ->delete("DELETE FROM nokta_uye_favoriler WHERE uye_id = $userId AND urun_id = $productId");
-            echo "removed";
-        } else {
-            $database ->insert("INSERT INTO nokta_uye_favoriler (uye_id, urun_id) VALUES ($userId, $productId)");
-            echo "added";
-        }
-    }
+$productId = $_POST['product_id'] ?? null;
+$userId = $_POST['uye_id'] ?? null;
+
+if (empty($userId)) {
+    header("Location: ../../tr/giris");
+    exit();
+}
+
+$checkStatement = $database->fetch("SELECT id FROM nokta_uye_favoriler WHERE uye_id = :userId AND urun_id = :productId", ['userId' => $userId,'productId' => $productId]);
+
+if ($checkStatement) { // fetch() sonucu null dönerse zaten favoriye eklenmemiştir
+    $database->delete("DELETE FROM nokta_uye_favoriler WHERE uye_id = :userId AND urun_id = :productId", ['userId' => $userId,'productId' => $productId]);
+    echo "removed";
+} else {
+    $database->insert("INSERT INTO nokta_uye_favoriler (uye_id, urun_id) VALUES (:userId, :productId)", ['userId' => $userId,'productId' => $productId]);
+    echo "added";
+}
 ?>
