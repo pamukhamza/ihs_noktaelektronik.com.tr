635f591aacf452dc5ea90db76d2947715b87c364
pamukhamza <hmzpmk34@gmail.com>
2025-03-06 14:50:12 +0300

    a

--- functions/siparis/sip_olustur.php
+++ functions/siparis/sip_olustur.php
@@ -464,7 +464,6 @@ if (isset($_GET['cariveri']) || isset($_GET['cariveriFinans'])) {
         $veri = base64_decode($_GET['cariveriFinans']);
     }
     $decodedVeri = json_decode($veri, true);
-    echo $decodedVeri;
     $yantoplam = $decodedVeri["yantoplam"];
     $cardNo = $decodedVeri["cardNo"];
     $cariOdeme = "cari";
