6bb4250e3e9a20edba02d7a2b3dabfedf86df6b4
pamukhamza <hmzpmk34@gmail.com>
2025-03-18 11:29:10 +0300

    a

---
+++ tr/deneme.php
@@ -0,0 +1,5 @@
+<form action="../functions/siparis/sip_olustur.php?sipFinans=eyJvZGVtZVRha3NpdCI6IjEiLCJ5YW5TZXBldFRvcGxhbWkiOiIwLjg4IiwieWFuU2VwZXRLZHYiOiIwLjE4IiwieWFuSW5kaXJpbSI6IjAuMDAiLCJ5YW5LYXJnbyI6IjAuMDAiLCJkZWxpdmVyeU9wdGlvbiI6IjAiLCJ5YW50b3BsYW0iOiIxLjA1IiwiZGVzaSI6IjAiLCJiYW5rYV9pZCI6IjEyMyIsInV5ZV9pZCI6IjE4NjAiLCJ0aXAiOiJTYW5hbCBQb3MiLCJsYW5nIjoidHIiLCJwcm9tb3N5b25fa29kdSI6bnVsbH0"
+ method="post">
+    <input type="hidden" name="mdStatus" value="1">
+    <button type="submit">Gönder</button>
+</form>
