939fbb16b3263a926e39f71ad57b0c3d79d0f08b
pamukhamza <hmzpmk34@gmail.com>
2025-03-19 11:38:11 +0300

    a

--- tr/siparis-detay.php
+++ tr/siparis-detay.php
@@ -1,7 +1,8 @@
 <?php
 require '../functions/admin_template.php';
 require '../functions/functions.php';
-
+ini_set('display_errors', 1);  // Hataları ekrana göster
+error_reporting(E_ALL);   
 $currentPage = 'siparis-detay';
 $template = new Template('Nokta Elektronik - Sipariş Detayı', $currentPage);
 
