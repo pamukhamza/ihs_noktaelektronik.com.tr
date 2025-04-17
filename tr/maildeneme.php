4004251be6eb09e6348ab1ca141a602b10b1312a
pamukhamza <hmzpmk34@gmail.com>
2025-04-17 12:50:59 +0300

    maildeneme

--- tr/maildeneme.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../mail/mail_gonder.php';
-
-$mail_icerik = uyeOnayMail('hamza Pamuk', 'hmzpmk34@gmail.com', '111111111111');
-mailGonder('hmzpmk34@gmail.com', 'Nokta Elektronik B2B Mail Deneme', 'deneme maildir', 'Nokta Elektronik B2B Mail Deneme');
-
