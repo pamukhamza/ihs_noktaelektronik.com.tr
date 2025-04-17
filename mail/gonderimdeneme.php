03163905adbaded109acf19c8e029e1c5ab59322
pamukhamza <hmzpmk34@gmail.com>
2025-04-17 12:52:13 +0300

    maildeneme

--- mail/gonderimdeneme.php
+++ mail/gonderimdeneme.php
@@ -1,26 +1,30 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
 function mailGonder($alici, $konu, $mesaj_icerik, $mailbaslik){
     include 'Exception.php';
     include 'PHPMailer.php';
     include 'SMTP.php';
     $mail = new PHPMailer(true);
 
-        //Server settings
-        $mail->SMTPDebug = 0; // Enable verbose debug output (set to 2 for maximum detail)
-        $mail->isSMTP(); // Set mailer to use SMTP
-        $mail->Host = 'mail.noktaelektronik.net'; // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true; // Enable SMTP authentication
-        $mail->Username = 'noktab2b@noktaelektronik.net'; // SMTP username
-        $mail->Password = 'Nktbb2023*'; // SMTP password
-        $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 465; // TCP port to connect to veya 587
+    try {
+        // Server settings
+        $mail->SMTPDebug = 2; // 0 = off, 2 = full debug output
+        $mail->isSMTP();
+        $mail->Host = 'mail.noktaelektronik.net';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'noktab2b@noktaelektronik.net';
+        $mail->Password = 'Nktbb2023*';
+        $mail->SMTPSecure = 'ssl'; // veya 'tls'
+        $mail->Port = 465;
         $mail->CharSet = 'UTF-8';
-        $mail->Encoding = 'base64'; 
-        //Recipients
+        $mail->Encoding = 'base64';
+
+        // Recipients
         $mail->setFrom('noktab2b@noktaelektronik.net', $mailbaslik);
-        $mail->addAddress($alici); // Add a recipient
+        $mail->addAddress($alici);
+
         if($konu == "Cari Ödeme Bildirimi"){
             $mail->addBCC("muhasebe@noktaelektronik.net");
         }
@@ -28,16 +32,22 @@ function mailGonder($alici, $konu, $mesaj_icerik, $mailbaslik){
             $mail->addBCC("h.pamuk@noktaelektronik.net");
             $mail->addBCC("kadir@noktaelektronik.net");
         }
-        //Content
-        $mail->Subject = $konu;
-        $mail->Body = "$mesaj_icerik";
-        // Set email format to HTML
-        $mail->isHTML(true);
-        // Try to send the email
-        try {
-            $mail->send();
-        } catch (Exception $e) {
-        }
-}
-mailGonder('hmzpmk34@gmail.com', 'Nokta Elektronik B2B Mail Deneme', 'deneme maildir', 'Nokta Elektronik B2B Mail Deneme');
 
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $konu;
+        $mail->Body    = $mesaj_icerik;
+
+        $mail->send();
+        echo "Mail başarıyla gönderildi.";
+    } catch (Exception $e) {
+        echo "Mail gönderilemedi. Hata: {$mail->ErrorInfo}";
+    }
+}
+
+mailGonder(
+    'hmzpmk34@gmail.com',
+    'Nokta Elektronik B2B Mail Deneme',
+    'deneme maildir',
+    'Nokta Elektronik B2B Mail Deneme'
+);
