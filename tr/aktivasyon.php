64057670d6c77e824c585b164028b82e86c3cb90
pamukhamza <hmzpmk34@gmail.com>
2025-04-18 15:08:51 +0300

    a

--- tr/aktivasyon.php
+++ tr/aktivasyon.php
@@ -1,10 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 include '../functions/db.php';
 $uye_id = $_GET['id'];
 $aktif = '1';
+$db = new Database();
 $update = $db->update("UPDATE uyeler SET aktivasyon = :aktif WHERE id = :id" , ['aktif' => $aktif, 'id' => $uye_id]);
 header("Location: giris?s=26");
 
