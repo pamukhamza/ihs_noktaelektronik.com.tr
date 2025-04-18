888bbfc6eba7f6815dbe408f555852e257fbf7f5
pamukhamza <hmzpmk34@gmail.com>
2025-04-18 14:50:45 +0300

    a

---
+++ tr/aktivasyon.php
@@ -0,0 +1,8 @@
+<?php
+include '../functions/db.php';
+$uye_id = $_GET['id'];
+$aktif = '1';
+$update = $db->update("UPDATE uyeler SET aktivasyon = :aktif WHERE id = :id" , ['aktif' => $aktif, 'id' => $uye_id]);
+header("Location: giris?s=26");
+
+?>
