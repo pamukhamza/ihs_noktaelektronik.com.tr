bc209ee3e356355f57d9cdb3737cbbcd4dfbd175
pamukhamza <hmzpmk34@gmail.com>
2025-04-18 12:21:33 +0300

    a

--- mail/maildene.php
+++ mail/maildene.php
@@ -1,12 +1,11 @@
 <?php
-include "../f.php";
-include "../function.php";
+include "mail_gonder.php";
 
 $uye = "hasan";
 $sip_id = "10";
 $siparis_no = "123";
 
-$mail = "ghasankececi@gmail.com";
+$mail = "hmzpmk34@gmail.com";
 
 $mail_icerik = siparisAlindi($uye, $sip_id, $siparis_no);
 mailGonder($mail, 'Siparis Deneme', $mail_icerik, 'Nokta');
