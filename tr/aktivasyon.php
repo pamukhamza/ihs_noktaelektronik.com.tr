f305cc0aaf8e964a303b0b46e7175e4f1bf34515
pamukhamza <hmzpmk34@gmail.com>
2025-04-24 13:55:22 +0300

    teknik

--- tr/aktivasyon.php
+++ tr/aktivasyon.php
@@ -3,7 +3,7 @@ include '../functions/db.php';
 $uye_id = $_GET['id'];
 $aktif = '1';
 $db = new Database();
-$update = $db->update("UPDATE uyeler SET aktivasyon = :aktif WHERE id = :id" , ['aktif' => $aktif, 'id' => $uye_id]);
+$update = $db->update("UPDATE uyeler SET aktivasyon = :aktif, aktif = :aktif WHERE id = :id" , ['aktif' => $aktif, 'id' => $uye_id]);
 header("Location: giris?s=26");
 
 ?>
