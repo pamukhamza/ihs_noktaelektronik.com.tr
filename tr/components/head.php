9258e4d3ee61796aebbb5df3762def1123051445
pamukhamza <hmzpmk34@gmail.com>
2025-04-24 12:43:04 +0300

    teknik

--- tr/components/head.php
+++ tr/components/head.php
@@ -1,4 +1,6 @@
 <?php 
+error_reporting(0);
+ini_set('display_errors', 0);
 $database = new Database();
 require_once __DIR__ . '/../../functions/uyeler/uye_giris.php';
 // Start the "Remember Me" functionality
