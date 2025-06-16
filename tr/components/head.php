2044653fa1e3311e9545a75f396f1027a73debe8
Hamza Pamuk <hmzpmk34@gmail.com>
2025-06-16 13:19:49 +0300

    favicon

--- tr/components/head.php
+++ tr/components/head.php
@@ -15,7 +15,7 @@ if (session_status() == PHP_SESSION_NONE) {
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/png" href="assets/images/logo_new.png">
+    <link rel="icon" type="image/png" href="assets/images/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="https://noktaelektronik.com.tr/">
     <title><?= $title; ?></title>
