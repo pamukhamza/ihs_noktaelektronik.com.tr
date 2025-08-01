57d0f67331156af410342426b81dd993b674c582
hamza Pamuk <hmzpmk34@gmail.com>
2025-08-01 16:50:43 +0300

    son kullanıcı fiyatları göremez

--- tr/components/head.php
+++ tr/components/head.php
@@ -11,7 +11,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 if (!isset($_SESSION['id'])) {
-    header("Location: ../giris");
+    header("Location: ../tr/giris");
     exit;
 }
 ?>
